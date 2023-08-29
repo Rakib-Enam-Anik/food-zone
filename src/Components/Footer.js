017d780065afd3cda2ce0cf06e0bbd8210ff1319
Md Tasib Haider <hridoyzylex@gmail.com>
2023-08-29 11:40:38 +0600

    fixed the footer issue

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,38 +7,36 @@ import { FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="footer-wrapper">
-      <div className="footer-section-one">
-        <div className="start">
-          <img src={Logo} alt="" />
-          <p className="text" >SendMeeting</p>
+    <div className="footer-wrapper-whole">
+      <div className="footer-wrapper">
+        <div className="footer-section-one">
+          <div className="start">
+            <img src={Logo} alt="" />
+            <p className="text" >SendMeeting</p>
+          </div>
+          <div className="footer-icons">
+            <BsTwitter />
+            <SiLinkedin />
+            <BsYoutube />
+            <FaFacebookF />
+          </div>
         </div>
-        <div className="footer-icons">
-          <BsTwitter />
-          <SiLinkedin />
-          <BsYoutube />
-          <FaFacebookF />
+        <div className="footer-section-two">
+          <div className="footer-section-columns">
+            <span>FAQ</span>
+            <span>Careers</span>
+            <span>About Us</span>
+            <span>Terms & Conditions</span>
+          </div>
+          <div className="footer-section-columns">
+            <span><a href="tel://+8801790826633">+8801790826633</a></span>
+            <span><a href="mailto://info@sendmeeting.com">info@sendmeeting.com</a></span>
+            <span>161/B (1st Floor) Lake Circus, <br /> Kalabagan, Dhaka</span>
+          </div>
         </div>
       </div>
-      <div className="footer-section-two">
-        <div className="footer-section-columns">
-          <span>Qualtiy</span>
-          <span>Help</span>
-          <span>Share</span>
-          <span>Carrers</span>
-          <span>Testimonials</span>
-          <span>Work</span>
-        </div>
-        <div className="footer-section-columns">
-          <span>244-5333-7783</span>
-          <span>hello@sendmeeting.com</span>
-          <span>press@sendmeeting.com</span>
-          <span>contact@sendmeeting.com</span>
-        </div>
-        <div className="footer-section-columns">
-          <span>Terms & Conditions</span>
-          <span>Privacy Policy</span>
-        </div>
+      <div align="center" className="footer-section-bottom">
+        <p>Design & Developed by <a href="http://www.teamexus.com" target="blank">Teamexus Solutions Ltd.</a> | Copyright © 2023 - All Rights Reserved.</p>
       </div>
     </div>
   );
